1256bc7420e886bddf5b547b94d3d05f36443829
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-07-06 18:15:15 -0300

    Start express app

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { errors } from "celebrate";
+
+import routes from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+export default app;
