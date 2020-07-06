ae3c5bc2138ba0079ab3a3d65a1fcc7dfc1db355
th92rodr <thiago.clrodrigues@gmail.com>
2020-07-06 18:18:53 -0300

    add route controller

---
+++ src/controllers/routes.controller.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+
+import AppError from "../errors/AppError";
+import RoutesRepository from "../repositories/routes.repository";
+import FindBestRouteService from "../service/FindBestRoute.service";
+
+export default class RoutesController {
+  public async create(request: Request, response: Response) {
+    try {
+      const { origem, destino, custo } = request.body;
+
+      const routesRepository = new RoutesRepository();
+
+      const route = routesRepository.create({
+        origem,
+        destino,
+        custo,
+      });
+
+      return response.json(route);
+    } catch (error) {
+      if (error instanceof AppError) {
+        return response
+          .status(error.statusCode)
+          .json({ message: error.message });
+      }
+      return response.status(500).json({ message: error.message });
+    }
+  }
+
+  public async findBestRoute(request: Request, response: Response) {
+    try {
+      const origem = request.query.origem as string;
+      const destino = request.query.destino as string;
+
+      const findBestRouteService = new FindBestRouteService();
+
+      const bestRoute: string = findBestRouteService.execute({
+        origem,
+        destino,
+      });
+
+      return response.json(bestRoute);
+    } catch (error) {
+      if (error instanceof AppError) {
+        return response
+          .status(error.statusCode)
+          .json({ message: error.message });
+      }
+      return response.status(500).json({ message: error.message });
+    }
+  }
+}
